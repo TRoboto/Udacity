fd85a19c43c0c6f8ef52d6b7ccf22de08ae3b6d7
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-05-29 14:01:15 +0300

    Complete

--- C++ Nanodegree/Project - Build an OpenStreetMap Route Planner/src/main.cpp
+++ C++ Nanodegree/Project - Build an OpenStreetMap Route Planner/src/main.cpp
@@ -56,11 +56,15 @@ int main(int argc, const char **argv)
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
 
+    std::cout << "Enter the start and end corrdinates, start_x, `start_y, end_x, end_y: ";
+    float start_x, start_y, end_x, end_y;
+    std::cin >> start_x >> start_y >> end_x >> end_y;
+
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
