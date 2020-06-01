7662909d6a785197e0dfb9ae8650e5f17c5b4ce4
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-01 23:40:02 +0300

    Complete

--- C++ Nanodegree/Project - System Monitor/include/processor.h
+++ C++ Nanodegree/Project - System Monitor/include/processor.h
@@ -3,10 +3,13 @@
 
 class Processor {
  public:
+  Processor();
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+ int iAll_;
+ int iIdle_;
 };
 
 #endif
--- C++ Nanodegree/Project - System Monitor/src/processor.cpp
+++ C++ Nanodegree/Project - System Monitor/src/processor.cpp
@@ -1,4 +1,20 @@
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+Processor::Processor(){
+  iAll_  = LinuxParser::Jiffies();
+  iIdle_ = LinuxParser::IdleJiffies();  
+}
+
+// TODO: Return the aggregate CPU utilization
+float Processor::Utilization() { 
+    
+  float fAllOld = iAll_;
+  float fIdleOld = iIdle_;  
+  iAll_  = LinuxParser::Jiffies();
+  iIdle_ = LinuxParser::IdleJiffies();
+
+  float rValue= (((iAll_-fAllOld)-(iIdle_-fIdleOld)) / (iAll_-fAllOld));
+  return (rValue>0.0)? rValue : 0.0;
+ }
