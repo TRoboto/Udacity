a686d649eec3c97a9e6349a35ef1aae42b08b0f6
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-01 22:09:53 +0300

    Update linux_parser.cpp

--- C++ Nanodegree/Project - System Monitor/src/linux_parser.cpp
+++ C++ Nanodegree/Project - System Monitor/src/linux_parser.cpp
@@ -241,7 +241,14 @@ int LinuxParser::RunningProcesses() {
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid [[maybe_unused]]) { return string(); }
+string LinuxParser::Command(int pid) { 
+  string line;
+  std::fstream stream{kProcDirectory + to_string(pid) + kCmdlineFilename};
+  if (stream.is_open()) {
+    std::getline(stream, line);
+  }
+  return line;
+ }
 
 // TODO: Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
