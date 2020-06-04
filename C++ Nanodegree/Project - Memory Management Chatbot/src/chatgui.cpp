8538ff888d48b4005dfcb17ae68b6aaf34e70eb5
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-05 00:46:30 +0300

    Task 1: Complete

--- C++ Nanodegree/Project - Memory Management Chatbot/src/chatgui.cpp
+++ C++ Nanodegree/Project - Memory Management Chatbot/src/chatgui.cpp
@@ -118,7 +118,7 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    _chatLogic = std::make_unique<ChatLogic>(); 
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
--- C++ Nanodegree/Project - Memory Management Chatbot/src/chatgui.h
+++ C++ Nanodegree/Project - Memory Management Chatbot/src/chatgui.h
@@ -2,6 +2,7 @@
 #define CHATGUI_H_
 
 #include <wx/wx.h>
+#include <memory>
 
 class ChatLogic; // forward declaration
 
@@ -16,7 +17,7 @@ private:
     //// STUDENT CODE
     ////
 
-    ChatLogic *_chatLogic;
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +28,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
