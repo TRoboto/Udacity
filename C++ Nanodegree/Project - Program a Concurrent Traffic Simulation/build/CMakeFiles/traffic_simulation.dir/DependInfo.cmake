8304354b911c5b756e994a7ae219d08ef6027886
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-07 01:33:22 +0300

    Sart

---
+++ C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/Graphics.cpp" "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.obj"
+  "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/Intersection.cpp" "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.obj"
+  "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/Street.cpp" "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.obj"
+  "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficLight.cpp" "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.obj"
+  "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficObject.cpp" "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.obj"
+  "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficSimulator-Final.cpp" "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.obj"
+  "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/Vehicle.cpp" "J:/GitHub repos/Udacity/C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/OpenCV-MinGW-Build-OpenCV-4.1.1-x64/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/build/CMakeFiles/traffic_simulation.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.obj"
+  "CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.obj"
+  "CMakeFiles/traffic_simulation.dir/src/Street.cpp.obj"
+  "CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.obj"
+  "CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.obj"
+  "CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.obj"
+  "CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.obj"
+  "libtraffic_simulation.dll.a"
+  "traffic_simulation.exe"
+  "traffic_simulation.exe.manifest"
+  "traffic_simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/traffic_simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
