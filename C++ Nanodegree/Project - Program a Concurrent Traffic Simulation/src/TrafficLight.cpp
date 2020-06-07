f2ae301ca06d4142626a1bcc0b31140db9a44a79
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-07 06:07:12 +0300

    FP.4a : complete

--- C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficLight.cpp
+++ C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficLight.cpp
@@ -5,7 +5,7 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+// /* 
 template <typename T>
 T MessageQueue<T>::receive()
 {
@@ -19,8 +19,12 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lock(_mutex);
+    _queue.emplace_back(msg);
+    _condition.notify_one();
+
 }
-*/
+// */
 
 /* Implementation of class "TrafficLight" */
 
