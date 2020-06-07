f81049eee1aa814dc7e91f2ed58d75d2a02d6021
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-07 05:57:18 +0300

    update

--- C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficLight.h
+++ C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficLight.h
@@ -30,7 +30,7 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-enum TrafficLightPhase {Green, Red};
+enum TrafficLightPhase {green = 0, red};
 
 class TrafficLight : TrafficObject
 {
@@ -50,7 +50,7 @@ private:
     // typical behaviour methods
     void cycleThroughPhases();
     TrafficLightPhase _currentPhase;
-    
+
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
