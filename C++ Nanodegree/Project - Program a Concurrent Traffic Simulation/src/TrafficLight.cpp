a61fd7b3306aa5c7956a4bd4286ea466073b5476
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-07 06:00:24 +0300

    FP.2b : complete

--- C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficLight.cpp
+++ C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficLight.cpp
@@ -45,6 +45,7 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases,this));
 }
 
 // virtual function which is executed in a thread
