346fc0ec7d1593cf0028ee93359e1c80bfa4eccc
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-07 06:04:45 +0300

    FP.3 : complete

--- C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficLight.h
+++ C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/TrafficLight.h
@@ -19,8 +19,13 @@ template <class T>
 class MessageQueue
 {
 public:
+    void send(T &&phase);
+    T receive();
 
 private:
+    std::deque<T> _queue;
+    std::condition_variable _condition;
+    std::mutex _mutex;
     
 };
 
