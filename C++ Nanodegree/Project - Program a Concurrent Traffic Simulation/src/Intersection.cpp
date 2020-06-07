63e4ac5ee97ed1c8046dfda7dff549d791bf5494
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-07 06:30:42 +0300

    FP.6a : complete

--- C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/Intersection.cpp
+++ C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/Intersection.cpp
@@ -109,7 +109,8 @@ void Intersection::setIsBlocked(bool isBlocked)
 void Intersection::simulate() // using threads + promises/futures + exceptions
 {
     // FP.6a : In Intersection.h, add a private member _trafficLight of type TrafficLight. At this position, start the simulation of _trafficLight.
-
+    _trafficLight.simulate();
+    
     // launch vehicle queue processing in a thread
     threads.emplace_back(std::thread(&Intersection::processVehicleQueue, this));
 }
--- C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/Intersection.h
+++ C++ Nanodegree/Project - Program a Concurrent Traffic Simulation/src/Intersection.h
@@ -6,6 +6,7 @@
 #include <mutex>
 #include <memory>
 #include "TrafficObject.h"
+#include "TrafficLight.h"
 
 // forward declarations to avoid include cycle
 class Street;
@@ -51,6 +52,7 @@ private:
     void processVehicleQueue();
 
     // private members
+    TrafficLight _trafficLight;
     std::vector<std::shared_ptr<Street>> _streets;   // list of all streets connected to this intersection
     WaitingVehicles _waitingVehicles; // list of all vehicles and their associated promises waiting to enter the intersection
     bool _isBlocked;                  // flag indicating wether the intersection is blocked by a vehicle
