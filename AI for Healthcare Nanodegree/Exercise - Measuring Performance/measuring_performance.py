adc620a930574dd9bcfbbd520c40c61b9cea0503
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-13 03:32:11 +0300

    Update measuring_performance.py

--- AI for Healthcare Nanodegree/Exercise - Measuring Performance/measuring_performance.py
+++ AI for Healthcare Nanodegree/Exercise - Measuring Performance/measuring_performance.py
@@ -19,11 +19,37 @@ if __name__ == "__main__":
     # spleen1_label_auto is the auto-generated one
     
     # <YOUR CODE HERE>
+    # __SOLUTION
+    lbl1 = nib.load("data/spleen1_label_auto.nii.gz").get_fdata()
+    lbl2 = nib.load("data/spleen1_label_gt.nii.gz").get_fdata()
+    # __SOLUTION
 
     # TASK: Now, implement two similarity metrics - sensitivity (assuming that gt is the True mask)
     # and Dice Similarity Coefficient. 
     # Hint: the formal measure of "set cardinality" that is featured in DSC definition could
     # be computed as simply the volume of your 3D object
 
-    # <YOUR CODE HERE> 
-    
+    # <YOUR CODE HERE>
+    # __SOLUTION
+    def dsc3d(a,b):
+        intersection = np.sum(a*b)
+        volumes = np.sum(a) + np.sum(b)
+
+        if volumes == 0:
+            return -1
+
+        return 2.*float(intersection) / float(volumes)
+
+    print(f"DSC: {dsc3d(lbl1, lbl2)}")
+
+    def sensitivity(gt,pred):
+        # Sens = TP/(TP+FN)
+        tp = np.sum(gt[gt==pred])
+        fn = np.sum(gt[gt!=pred])
+
+        if fn+tp == 0:
+            return -1
+
+        return (tp)/(fn+tp)
+
+    print(f"Sensitivity: {sensitivity(lbl1, lbl2)}")
