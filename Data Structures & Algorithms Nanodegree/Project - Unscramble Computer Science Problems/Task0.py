a7d0e650ebf99806987bdcaa9586e92621e6cce8
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-28 01:11:11 +0300

    Update Task0.py

--- Data Structures & Algorithms Nanodegree/Project - Unscramble Computer Science Problems/Task0.py
+++ Data Structures & Algorithms Nanodegree/Project - Unscramble Computer Science Problems/Task0.py
@@ -20,3 +20,5 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+print(f"First record of texts, {texts[0][0]} texts {texts[0][1]} at time {texts[0][2]}")
+print(f"Last record of calls, {calls[-1][0]} calls {calls[-1][1]} at time {calls[-1][2]}, lasting {calls[-1][3]} seconds")
