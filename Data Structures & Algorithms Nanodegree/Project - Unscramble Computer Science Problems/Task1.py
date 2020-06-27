9f0307405c4b8e31bf143bc0c9d034a40ee6987a
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-28 01:24:35 +0300

    Update Task1.py

--- Data Structures & Algorithms Nanodegree/Project - Unscramble Computer Science Problems/Task1.py
+++ Data Structures & Algorithms Nanodegree/Project - Unscramble Computer Science Problems/Task1.py
@@ -18,3 +18,14 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+unique_phones = set()
+for record in texts:
+	unique_phones.add(record[0])
+	unique_phones.add(record[1])
+	
+for record in calls:
+	unique_phones.add(record[0])
+	unique_phones.add(record[1])
+	
+print(f"There are {len(unique_phones)} different telephone numbers in the records.")
