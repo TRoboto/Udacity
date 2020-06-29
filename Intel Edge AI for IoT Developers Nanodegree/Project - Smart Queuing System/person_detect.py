ffcd793487008d71d5daf9ce87e35ae8b7ae3e88
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-06-30 01:29:46 +0300

    Add

---
+++ Intel Edge AI for IoT Developers Nanodegree/Project - Smart Queuing System/person_detect.py
@@ -0,0 +1,187 @@
+
+import numpy as np
+import time
+from openvino.inference_engine import IENetwork, IECore
+import os
+import cv2
+import argparse
+import sys
+
+
+class Queue:
+    '''
+    Class for dealing with queues
+    '''
+    def __init__(self):
+        self.queues=[]
+
+    def add_queue(self, points):
+        self.queues.append(points)
+
+    def get_queues(self, image):
+        for q in self.queues:
+            x_min, y_min, x_max, y_max=q
+            frame=image[y_min:y_max, x_min:x_max]
+            yield frame
+    
+    def check_coords(self, coords):
+        d={k+1:0 for k in range(len(self.queues))}
+        for coord in coords:
+            for i, q in enumerate(self.queues):
+                if coord[0]>q[0] and coord[2]<q[2]:
+                    d[i+1]+=1
+        return d
+
+
+class PersonDetect:
+    '''
+    Class for the Person Detection Model.
+    '''
+
+    def __init__(self, model_name, device, threshold=0.60):
+        self.model_weights=model_name+'.bin'
+        self.model_structure=model_name+'.xml'
+        self.device=device
+        self.threshold=threshold
+
+        try:
+            self.model=IENetwork(self.model_structure, self.model_weights)
+        except Exception as e:
+            raise ValueError("Could not Initialise the network. Have you enterred the correct model path?")
+
+        self.input_name=next(iter(self.model.inputs))
+        self.input_shape=self.model.inputs[self.input_name].shape
+        self.output_name=next(iter(self.model.outputs))
+        self.output_shape=self.model.outputs[self.output_name].shape
+
+    def load_model(self):
+        self.net = IECore().load_network(network=self.model, device_name=self.device)
+        
+    def predict(self, image):
+        processed_image = self.preprocess_inputs(image)
+        self.net.start_async(0, inputs = {self.input_name:processed_image})
+        if self.net.requests[0].wait()==0:
+            result = self.net.requests[0].outputs[self.output_name]
+                
+        outputs = self.preprocess_outputs(result, image)
+        image = self.draw_outputs(outputs, image)
+
+        return outputs, image
+    
+    def draw_outputs(self, coords, image):
+        frame = image.copy()
+        for data in coords:
+            cv2.rectangle(frame, data[:2], data[2:], (0, 255, 0), 2 )
+        
+        return frame
+
+    def preprocess_outputs(self, outputs, image):
+        h, w = image.shape[0:2]
+        coords = []
+        for data in outputs[0][0]:
+            # Draw bounding box for object
+            if data[2] >= self.threshold:
+                xmin = int(data[3] * w)
+                ymin = int(data[4] * h)
+                xmax = int(data[5] * w)
+                ymax = int(data[6] * h)
+                coords.append((xmin, ymin, xmax, ymax))
+                
+        return coords
+    
+    
+    def preprocess_inputs(self, image):
+        p_frame = cv2.resize(image, (self.input_shape[3], self.input_shape[2]))
+        p_frame = p_frame.transpose(2, 0, 1)
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+        return p_frame
+
+
+def main(args):
+    model=args.model
+    device=args.device
+    video_file=args.video
+    max_people=args.max_people
+    threshold=args.threshold
+    output_path=args.output_path
+
+    start_model_load_time=time.time()
+    pd= PersonDetect(model, device, threshold)
+    pd.load_model()
+    total_model_load_time = time.time() - start_model_load_time
+
+    queue=Queue()
+    
+    try:
+        queue_param=np.load(args.queue_param)
+        for q in queue_param:
+            queue.add_queue(q)
+    except:
+        print("error loading queue param file")
+
+    try:
+        cap=cv2.VideoCapture(video_file)
+    except FileNotFoundError:
+        print("Cannot locate video file: "+ video_file)
+    except Exception as e:
+        print("Something else went wrong with the video file: ", e)
+    
+    initial_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    initial_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    video_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    out_video = cv2.VideoWriter(os.path.join(output_path, 'output_video.mp4'), cv2.VideoWriter_fourcc(*'avc1'), fps, (initial_w, initial_h), True)
+    
+    counter=0
+    start_inference_time=time.time()
+
+    try:
+        while cap.isOpened():
+            ret, frame=cap.read()
+            if not ret:
+                break
+            counter+=1
+            
+            coords, image= pd.predict(frame)
+            num_people= queue.check_coords(coords)
+            print(f"Total People in frame = {len(coords)}")
+            print(f"Number of people in queue = {num_people}")
+            out_text=""
+            y_pixel=25
+            
+            for k, v in num_people.items():
+                out_text += f"No. of People in Queue {k} is {v} "
+                if v >= int(max_people):
+                    out_text += f" Queue full; Please move to next Queue "
+                cv2.putText(image, out_text, (15, y_pixel), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                out_text=""
+                y_pixel+=40
+            out_video.write(image)
+            
+        total_time=time.time()-start_inference_time
+        total_inference_time=round(total_time, 1)
+        fps=counter/total_inference_time
+
+        with open(os.path.join(output_path, 'stats.txt'), 'w') as f:
+            f.write(str(total_inference_time)+'\n')
+            f.write(str(fps)+'\n')
+            f.write(str(total_model_load_time)+'\n')
+
+        cap.release()
+        cv2.destroyAllWindows()
+    except Exception as e:
+        print("Could not run Inference: ", e)
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--model', required=True)
+    parser.add_argument('--device', default='CPU')
+    parser.add_argument('--video', default=None)
+    parser.add_argument('--queue_param', default=None)
+    parser.add_argument('--output_path', default='/results')
+    parser.add_argument('--max_people', default=2)
+    parser.add_argument('--threshold', default=0.60)
+    
+    args=parser.parse_args()
+
+    main(args)
---
+++ Intel Edge AI for IoT Developers Nanodegree/Project - Smart Queuing System/queue_job.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+exec 1>/output/stdout.log 2>/output/stderr.log
+
+# TODO: Create MODEL variable
+# TODO: Create DEVICE variable
+# TODO: Create VIDEO variable
+MODEL=$1
+DEVICE=$2
+VIDEO=$3
+QUEUE=$4
+OUTPUT=$5
+# TODO: Create PEOPLE variable
+PEOPLE=$6
+mkdir -p $5
+
+if echo "$DEVICE" | grep -q "FPGA"; then # if device passed in is FPGA, load bitstream to program FPGA
+    #Environment variables and compilation for edge compute nodes with FPGAs
+    source /opt/intel/init_openvino.sh
+    aocl program acl0 /opt/intel/openvino/bitstreams/a10_vision_design_sg1_bitstreams/2019R4_PL1_FP16_MobileNet_Clamp.aocx
+fi
+
+python3 person_detect.py  --model ${MODEL} \
+                          --device ${DEVICE} \
+                          --video ${VIDEO} \
+                          --queue_param ${QUEUE} \
+                          --output_path ${OUTPUT}\
+                          --max_people ${PEOPLE} \
+
+cd /output
+
+tar zcvf output.tgz *
