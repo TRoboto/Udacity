f030049e96d68b3697af03e314a58d8ed60ca812
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-10-03 16:30:35 +0300

    Update app.py

--- Full Stack Developer Nanodegree/Capstone Project - Casting Agency/app.py
+++ Full Stack Developer Nanodegree/Capstone Project - Casting Agency/app.py
@@ -1,6 +1,4 @@
-import os
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 from auth.auth import requires_auth, AuthError
@@ -229,4 +227,4 @@ def create_app(test_config=None):
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run(debug=True)
