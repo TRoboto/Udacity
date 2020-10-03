624994a5d1ea7f567221012077cc4a08976b0f73
Mohammad Al-Fetyani <m4bh@hotmail.com>
2020-10-03 12:30:46 +0300

    Update app.py

--- Full Stack Developer Nanodegree/Capstone Project - Casting Agency/app.py
+++ Full Stack Developer Nanodegree/Capstone Project - Casting Agency/app.py
@@ -3,12 +3,14 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
 
-  return app
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
+
+    return app
+
 
 APP = create_app()
 
